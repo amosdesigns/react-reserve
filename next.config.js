882d47707295c3b1188d441ec0a4f315dde2afd9
jerome amos <mail@amosdesigns.net>
2021-09-02 21:32:08 -0400

    working homepage and connect to mongoose db
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: `${process.env.DB_HOST}://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfoht.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`,
+    MONGO_SRV: `${process.env.DB_HOST}://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfoht.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     JWT_SECRET: process.env.JWT_SECRET,
     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import ProductList from "../components/index/ProductList";
+function Home({products}) {
+  
 
-function Home(props) {
-  // console.table(props);
-  // useEffect(() => {
-    // getProducts();
-  // }, []);
-
-  // async function getProducts() {
-    // const url = "http://localhost:3000/api/products";
-    // const response = await axios.get(url);
-    // console.table(response.data);
-  // }
-
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 Home.getInitialProps = async () => {
   // fetch data
