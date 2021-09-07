b14d2a4f487694a8f0b2ad78b5e7ebc6a2b7835b
jerome amos <mail@amosdesigns.net>
2021-09-07 17:22:47 -0400

    working product pages
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import ProductList from "../components/index/ProductList";
-function Home({products}) {
-  
-
+import baseUrl from "../utils/baseUrl";
+function Home({ products }) {
   return <ProductList products={products} />;
 }
 Home.getInitialProps = async () => {
   // fetch data
-  const url = "http://localhost:3000/api/products";
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
 };
